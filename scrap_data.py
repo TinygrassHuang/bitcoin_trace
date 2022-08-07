5418f259d7d29af335e2bb82688a441b6a5bcb5b
Zhiping Huang <zh1516@ic.ac.uk>
2022-08-07 02:36:30 +0100

    scraping script for new training data

---
+++ scrap_data.py
@@ -0,0 +1,41 @@
+# total 10000 addresses:
+# 5 classes, 2000 addr per class, 10 entities per class
+# 200 addr per entity
+
+
+# Exchanges:
+"""
+Huobi.com, Huobi.com-2, Bittrex.com, Luno.com, Poloniex.com,
+Kraken.com, Binance.com, Binance.com-old
+Bitstamp.net, Cex.io
+"""
+
+# Pools:
+"""
+BTCCPool, SlushPool.com, SlushPool.com-old, SlushPool.com-old2
+AntPool.com, AntPool.com-old, AntPool.com-old2
+BitMinter.com, GHash.io, BW.com
+"""
+
+# Services/others:
+"""
+CoinPayments.net, Xapo.com, Cryptonator.com, Cubits.com, HaoBTC.com
+HelixMixer, BTCJam.com, GreenRoadMarket, NucleusMarket
+HelixMixer-old
+"""
+
+# Gambling:
+"""
+SatoshiDice.com, SatoshiDice.com-original, BitcoinVideoCasino.com, 
+NitrogenSports.eu, CoinGaming.io, SatoshiMines.com
+PrimeDice.com, Betcoin.ag, CoinRoyale.com, SwCPoker.eu
+"""
+
+# Darknet Market:
+"""
+AgoraMarket, SilkRoadMarketplace, SilkRoad2Market, SheepMarketplace *2, 
+MiddleEarthMarketplace, CannabisRoadMarket, PandoraOpenMarket,
+AbraxasMarket, BlueSkyMarketplace 
+"""
+
+
