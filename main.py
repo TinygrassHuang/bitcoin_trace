84707ee6a625ad7f9e9dfa234b9367b6e5bfa6b4
Zhiping Huang <zh1516@ic.ac.uk>
2022-08-23 18:11:28 +0100

    main script to run whole software

--- main.py
+++ main.py
@@ -1,130 +1,43 @@
-from bs4 import BeautifulSoup
-import requests, time
+import time
 
-# request interval: 1s(sometimes) 2s(okay)
-
-# 3LaNNTg87XjTtXAqs55WV5DyWASEZizCXZ
-# 1JdzjkxN9pAhmfRT6148UHsAPLM4QPYPqu
-# 1Lm8VUCnqUFy6CcQyntcc3kd9o949UPR9f BlackBankMarket
-bitcoin_address = "1JdzjkxN9pAhmfRT6148UHsAPLM4QPYPqu"
-url = 'https://www.walletexplorer.com/address/' + bitcoin_address
-html = requests.get(url)
-print(html)
-html_text = html.text
-soup = BeautifulSoup(html_text, 'lxml')
-
-wallet = soup.find('div', class_='walletnote').a.text
-print(wallet)
-wallet = wallet[1:-1]
-print(wallet)
-
-transactions = soup.find_all('tr', {'class': ['sent', 'received']})
-
-for trans in transactions:
-    date = trans.find('td', class_='date').text
-    amount_diff = trans.find('td', class_='amount diff').text
-    balance = trans.find('td', class_='amount').next_sibling.text
-    txid = trans.find('td', class_='txid').text
-    # print(date,amount_diff,balance,txid)
-
-print(transactions[0].text)
-print(len(transactions))
-# date = transactions[0].find('td', class_='date').text
-# amount_diff = transactions[0].find('td', class_='amount diff').text
-# balance = transactions[0].find('td', class_='amount').next_sibling.text
-# txid = transactions[0].find('td', class_='txid').text
-# print(date,amount_diff,balance,txid)
-
-txid = "6ec70ac1f3ad95047a47972997f93ad35a21d34700b0a7c481e5a5fac6c559fc"
-tx_url = "https://www.walletexplorer.com/txid/" + txid
-tx_html = requests.get(tx_url).text
-tx_soup = BeautifulSoup(tx_html, "lxml")
-table = tx_soup.find_all("table", class_="empty")
-input_list = table[0]
-output_list = table[1]
-
-info = tx_soup.find("table", class_="info")
-print(info("td")[2].text)  # time
-print(info("td")[3].text)  # sender
-print(info("td")[4].text)  # fee
-
-for member in output_list("tr"):
-    info = member("td")
-    addr = info[0].a.text
-    wallet = info[1].text
-    if wallet[0] == "[":
-        wallet = wallet[1:-1]
-    amount = info[2].text[:-3]
-    if info[3].text == "unspent":
-        flow = info[3].text
-    else:
-        flow = info[3].a["href"][6:]
-    print(addr)
-    print(wallet)
-    print(amount)
-    print(flow)
-
-for member in input_list("tr"):
-    info = member("td")
-    addr = info[0].a.text
-    amount = info[1].text[:-3]
-    flow = info[2].a["href"][6:]
-    print(addr)
-    print(amount)
-    print(flow)
-
-abc = tx_soup.find("table", class_="tx").span.text
-print(f"......{abc[1:-5]}......")
+from postprocess import Postprocessor
+from tracer import BitcoinTracer
 
 
-# print(output_list.find_all("tr")[0].text)
-# print(output_list.find_all("td")[0])
-# print(output_list.find_all("td")[1])
-# print(output_list.find_all("td")[2])
-# print(output_list.find_all("td")[3])
-# i = 0
-# bitcoin_address = "1JdzjkxN9pAhmfRT6148UHsAPLM4QPYPqu"
-# url = 'https://www.walletexplorer.com/address/' + bitcoin_address
-# start = time.time()
-# while(i < 60):
-#     html_text = requests.get(url)
-#     print(html_text)
-#     i += 1
-#     print(i)
-#     # time.sleep(1)
-# end = time.time()
-# print(end - start)
+def run_programme(addr, trace_depth, min_amount):
+    """ ------------------ Trace ------------------------"""
+    tic = time.perf_counter()  # start timer
+    tracer = BitcoinTracer(addr, trace_depth, min_amount=min_amount)  # initialise tracer
+    [source_addr, amount] = tracer.trace()  # construct network and get the source addresses
+    for i in range(len(source_addr)):
+        print(source_addr[i], amount[i])
+    print("total number of source:", len(source_addr))
+    toc = time.perf_counter()  # stop timer
+    print(f"Time spent {toc - tic:0.4f} seconds")
 
-def try_this():
-    a = [1, 2, 3, 4, 5]
-    b = [1, 1, 1, 1]
-    return a, b
+    """ ------------------ Classify ------------------------"""
+    tracer.classify_all_result_addr()
+    tac = time.perf_counter()  # stop timer
+    print(f"Time spent {tac - toc:0.4f} seconds")
+    tracer.save_results()
 
-
-[a,b] = try_this()
-print(a)
-print(b)
-print([1,2,3,4] + [5,6,7,8])
+    """ ------------------ Process ------------------------"""
+    pp = Postprocessor(addr, trace_depth,min_amount)
+    pp.print_data()
+    pp.print_data_info()
+    pp.plot(save=True)
 
 
-value = [1, 2, 3, 4, 5, 10]
-key = ["a", "b", "c", "d", "e","a"]
-diction = {}
-for i in range(len(key)):
-    if key[i] in diction:
-        diction[key[i]] = diction[key[i]] + value[i]
-    else:
-        diction[key[i]] = value[i]
+if __name__ == "__main__":
+    # bc1qmxjefnuy06v345v6vhwpwt05dztztmx4g3y7wp another very rich address with 8 unspent
+    # "1ArJTD4iR3SCwjuyiP3H6pbwvPBHduX6WB" # bitpay.com
 
-print(diction)
-
-# url = "https://www.walletexplorer.com/api/1/tx?txid=3870a9c90877dd5655861d52fe57effabb29ca490a15d3c510c79140f5052bb0&caller=zhiping.huang16@imperial.ac.uk"
-# for i in range(100):
-#     print(requests.get(url))
-#     time.sleep(0.5)
-
-a = []
-a.append(["a",1])
-a.append(["b",2])
-print([i[1] for i in a])
-
+    address = "1ArJTD4iR3SCwjuyiP3H6pbwvPBHduX6WB"
+    min_BTC = 0.001  # 0.00000001 -- 1 statoshi
+    depth_begin = 5
+    depth_end = 10
+    depth_list = range(depth_begin, depth_end+1)  # include end
+    depth_list = [4,5,6,7,8,9,10,12,15,20]
+    # run programme with all the tracing depth values
+    for i in depth_list:
+        run_programme(address, i, min_BTC)
